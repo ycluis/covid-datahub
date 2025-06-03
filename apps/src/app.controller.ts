b6cff871bba6591c2e1f9b89ee4e71fd819dbdf3
ycluis <ycluisdev@gmail.com>
2025-06-03 21:08:43 +0800

    feat: enhance data synchronization

--- apps/src/app.controller.ts
+++ apps/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DataSyncService } from './data-sync/data-sync.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(
+    private readonly appService: AppService,
+    private readonly dataSyncService: DataSyncService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('sync')
+  async syncData() {
+    const result = await this.dataSyncService.handleCron();
+    return JSON.stringify(result);
   }
 }
